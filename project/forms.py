641111aeb94212196184de7cafdf2c2e870a024d
Richard Wartenburger <richard.wartenburger@gmail.com>
2017-11-28 14:29:35 +0100

    FlaskTaskr, Part 2 - implemented user registration and login

--- project/forms.py
+++ project/forms.py
@@ -1,9 +1,9 @@
 # project/forms.py
 
 
-from flask_wtf import Form
-from wtforms import StringField, DateField, IntegerField, SelectField
-from wtforms.validators import DataRequired
+from FlaskForm import Form
+from wtforms import StringField, DateField, IntegerField, SelectField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class AddTaskForm(Form):
@@ -25,3 +25,31 @@ class AddTaskForm(Form):
         ]
     )
     status = IntegerField('Status')
+
+class UserRegistration(Form):
+    name = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=6, max=25)]
+    )
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Length(min=6, max=40)]        
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    confirm = PasswordField(
+        'Repeat Password',
+        validators=[DataRequired(), EqualTo('password', message='Passwords must match')]
+    )
+
+class LoginForm(Form):
+    name = StringField(
+        'Username',
+        validators=[DataRequired()]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired()]
+    )
