13bcb1d0b8c9561d08733a4b70d3d653ed93d3a9
Richard Wartenburger <richard.wartenburger@gmail.com>
2017-11-28 15:45:31 +0100

    Tasks associated with users

--- project/forms.py
+++ project/forms.py
@@ -1,12 +1,12 @@
 # project/forms.py
 
 
-from FlaskForm import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, IntegerField, SelectField, PasswordField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 
-class AddTaskForm(Form):
+class AddTaskForm(FlaskForm):
     task_id = IntegerField()
     name = StringField(
         'Task Name',
@@ -26,7 +26,7 @@ class AddTaskForm(Form):
     )
     status = IntegerField('Status')
 
-class UserRegistration(Form):
+class RegisterForm(FlaskForm):
     name = StringField(
         'Username',
         validators=[DataRequired(), Length(min=6, max=25)]
@@ -44,7 +44,7 @@ class UserRegistration(Form):
         validators=[DataRequired(), EqualTo('password', message='Passwords must match')]
     )
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     name = StringField(
         'Username',
         validators=[DataRequired()]
